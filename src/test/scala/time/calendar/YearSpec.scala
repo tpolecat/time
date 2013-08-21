19339794999e44a30caa081f8a422de41b859b55
Rob Norris <rob_norris@mac.com>
2013-08-21 10:14:10 -0700

    more tests

--- src/test/scala/time/calendar/YearSpec.scala
+++ src/test/scala/time/calendar/YearSpec.scala
@@ -3,8 +3,10 @@ package calendar
 
 import org.scalacheck.Arbitrary
 import org.scalacheck.Arbitrary._
+import org.scalacheck.Prop._
 import org.specs2.scalaz._
 import scalaz.scalacheck.ScalazProperties._
+import scalaz.Equal
 
 class YearSpec extends Spec {
 
@@ -14,5 +16,17 @@ class YearSpec extends Spec {
   
   checkAll(equal.laws[Year])
   checkAll(enum.laws[Year])
+
+  "catamorphism must satisfy" >> {
+
+    "identity" ! forAll { (y: Year) => 
+      Equal[Year].equal(y.fold(Year.apply, Year.apply), y) 
+    }
+
+    "partition" ! forAll { (y: Year) =>
+      y.fold(!Year.isLeapYear(_), Year.isLeapYear)
+    }
+
+  }
 
 }
